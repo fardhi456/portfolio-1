699f4d9f3de6698d8c6b5d9ede094877c3f14f5d
fardhi456 <fardheenkpofficial@gmail.com>
2025-08-11 07:45:33 +0400

    Remove unused Link import to fix Vercel build

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { blogs } from '@/contents/blogs'
-import Link from 'next/link'
 import { FaCalendarAlt, FaClock } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { fadeInUp, staggerContainer, cardHoverSmall } from '@/utils/animations'
